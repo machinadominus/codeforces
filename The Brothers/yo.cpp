9e05b779b7b77bf373259b38c6136a7cc670c566
sohail <mohammedsohail98201@gmail.com>
2024-02-02 18:00:07 +0530

    Create yo.cpp

---
+++ The Brothers/yo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+  string a, b, c, d;
+  cin >> a >> b >> c >> d;
+  if (b == d){
+    printf("ARE Brothers\n");
+  }
+  else {
+    printf("NOT\n");
+  }
+  return 0;
+}
