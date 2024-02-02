95e3df9c4753d858b106b16b060e170655078614
sohail <mohammedsohail98201@gmail.com>
2024-02-02 16:43:38 +0530

    Create yo.cpp

---
+++ Simple_Calculator/yo.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]){
+  long long a, b;
+  cin >> a >> b;
+  cout << a << " + " << b << " = " << a + b << "\n";
+  cout << a << " * " << b << " = " << a * b << "\n";
+  cout << a << " - " << b << " = " << a - b << "\n";
+
+  return 0;
+}
