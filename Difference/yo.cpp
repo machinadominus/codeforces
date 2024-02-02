9f49a628794f8897921996906a1d93788a9c377d
sohail <mohammedsohail98201@gmail.com>
2024-02-02 16:49:52 +0530

    Create yo.cpp

---
+++ Difference/yo.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char* argv[]){
+  long long a, b, c, d;
+  cin >> a >> b >> c >> d;
+  cout << "Difference = " << a * b - c * d << "\n";
+  return 0;
+}
